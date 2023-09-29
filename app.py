07469a6aa9b890d496b1aa3af5da52d837e7e1a9
yrnThiago <thiagodias_123@hotmail.com>
2023-09-29 16:01:45 -0300

    feat: add home, quemsomos, contato routes

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+reasons = ["Material sempre atualizado", "Tablets em sala de aula", "Professores renomados",
+           "Universidade bem avaliada", "Ótima localização"]
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, Flask!</p>"
+
+
+@app.route('/home')
+def home():
+    return render_template('Home.html')
+
+
+@app.route("/quemsomos")
+def quemsomos():
+    return render_template("QuemSomos.html", qty_reasons=len(reasons), reasons=reasons)
+
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
