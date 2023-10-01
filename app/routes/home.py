d7c7dcc8832c63e231652df06c1bc3d53bc04a38
yrnThiago <thiagodias_123@hotmail.com>
2023-10-01 19:22:02 -0300

    feat: add redirect route

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from app.components.base_title_text import base_title_text
 from app.constants.home import ABOUT_TEXT
 
 home_blueprint = Blueprint('home', __name__)
 
 
-@home_blueprint.route('/')
 @home_blueprint.route('/home')
 def home():
     return render_template('home.html', about_university=base_title_text(ABOUT_TEXT))
+
+
+@home_blueprint.route('/')
+def index():
+    return redirect(url_for('home.home'))
