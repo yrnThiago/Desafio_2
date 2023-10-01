467a441591bca5931cf5cfe1cac832b3b9f165c7
yrnThiago <thiagodias_123@hotmail.com>
2023-10-01 10:28:28 -0300

    feat: add base text component

---
+++ components/base_title_text.py
@@ -0,0 +1,7 @@
+
+def base_title_text(text_info: dict) -> str:
+    title_text_html = f"<h1>{text_info.get('title')}</h1>"
+    for p in text_info.get('text'):
+        title_text_html += f"<p>{p}</p>"
+
+    return title_text_html
