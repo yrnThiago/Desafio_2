9b6b48979a1d838b7bfc77fb001c5e9dd9cc91ca
yrnThiago <thiagodias_123@hotmail.com>
2023-10-01 12:45:47 -0300

    fix: refactor app

--- app.py
+++ app.py
@@ -1,31 +1,7 @@
-from flask import Flask, render_template
-from components.input_forms import form_inputs_html
-from components.base_title_text import base_title_text
-from constants.QuemSomos import reasons_to_study, ABOUT_US_TEXT, OBJECTIVES_TEXT
-from constants.Home import ABOUT_TEXT
-from constants.Contato import CONTACT_TEXT
+from app import create_app
 
-app = Flask(__name__)
-
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('Home.html', about_university=base_title_text(ABOUT_TEXT))
-
-
-@app.route("/quemsomos")
-def quemsomos():
-    return render_template("QuemSomos.html", about_us=base_title_text(ABOUT_US_TEXT),
-                           qty_reasons=len(reasons_to_study), reasons=reasons_to_study,
-                           objectives=base_title_text(OBJECTIVES_TEXT))
-
-
-@app.route("/contato")
-def contato():
-    return render_template("contato.html", contact=base_title_text(CONTACT_TEXT),
-                           inputs_forms=form_inputs_html)
-
+config_name = "development"
+app = create_app(config_name)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
