5f6dae235503a81243c1f79a751e8d5d08f44883
yrnThiago <thiagodias_123@hotmail.com>
2023-10-01 12:45:32 -0300

    feat: add config files

---
+++ config/config.py
@@ -0,0 +1,25 @@
+class Config:
+    """
+    Configuração base
+    """
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    """
+    Configuração para ambiente de desenvolvimento
+    """
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """
+    Configuração para ambiente de produção
+    """
+    DEBUG = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+}
