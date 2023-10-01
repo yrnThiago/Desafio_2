bc04fec3ade369a0026f699ed1cb3138f3c3fdbe
yrnThiago <thiagodias_123@hotmail.com>
2023-10-01 10:31:07 -0300

    fix: refactor code

--- app.py
+++ app.py
@@ -1,51 +1,30 @@
 from flask import Flask, render_template
+from components.input_forms import form_inputs_html
+from components.base_title_text import base_title_text
+from constants.QuemSomos import reasons_to_study, ABOUT_US_TEXT, OBJECTIVES_TEXT
+from constants.Home import ABOUT_TEXT
+from constants.Contato import CONTACT_TEXT
 
 app = Flask(__name__)
-
-reasons_to_study = ["Material sempre atualizado", "Tablets em sala de aula", "Professores renomados",
-                    "Universidade bem avaliada", "Ótima localização"]
-
-inputs_forms = [
-    {"tag": "input", "title": "Seu e-mail", "type": "email", "id": "email", "placeholder": "seuemail@email.com"},
-    {"tag": "input", "title": "Assunto", "type": "text", "id": "assunto", "placeholder": "Assunto"},
-    {"tag": "textarea", "title": "Descrição", "rows": "2", "cols": "29", "id": "descricao", "placeholder": "Isso é uma descrição..."},
-]
-
-
-def convert_to_input_form(form_inputs):
-    form_inputs_html, input_element = "", ""
-    for form_input in form_inputs:
-        if form_input.get("tag") == "input":
-            input_element = f"""<input class="formulario__input" type="{form_input.get('type')}"
-             id="{form_input.get('id')}" placeholder="{form_input.get('placeholder')}">"""
-        else:
-            input_element = f"""<textarea rows="{form_input.get('rows')}" cols="{form_input.get('cols')}"
-             id="{form_input.get('id')}" placeholder="{form_input.get('placeholder')}"></textarea>"""
-
-        form_inputs_html += f"""
-            <div class="formulario__{form_input.get('type')}">
-                <label for="{form_input.get('type')}">{form_input.get('title')}:</label><br>
-                {input_element}
-            </div>
-        """
-
-    return form_inputs_html
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('Home.html')
+    return render_template('Home.html', about_university=base_title_text(ABOUT_TEXT))
 
 
 @app.route("/quemsomos")
 def quemsomos():
-    return render_template("QuemSomos.html", qty_reasons=len(reasons_to_study), reasons=reasons_to_study)
+    return render_template("QuemSomos.html", about_us=base_title_text(ABOUT_US_TEXT),
+                           qty_reasons=len(reasons_to_study), reasons=reasons_to_study,
+                           objectives=base_title_text(OBJECTIVES_TEXT))
 
 
 @app.route("/contato")
 def contato():
-    return render_template("contato.html", inputs_forms=convert_to_input_form(inputs_forms))
+    return render_template("contato.html", contact=base_title_text(CONTACT_TEXT),
+                           inputs_forms=form_inputs_html)
 
 
 if __name__ == "__main__":
