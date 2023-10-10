1ec25b5472289b709fccbb37990d18f46dc55f8d
yrnThiago <thiagodias_123@hotmail.com>
2023-10-09 21:55:42 -0300

    fix: refactor useful model funcs

--- app/models/Contato.py
+++ app/models/Contato.py
@@ -11,27 +11,3 @@ class Contato(db.Model):
         self.email = email
         self.assunto = assunto
         self.descricao = descricao
-
-    def adicionar(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self, new_contact: dict):
-        self.email = new_contact.get("email")
-        self.assunto = new_contact.get("assunto")
-        self.descricao = new_contact.get("descricao")
-
-        db.session.commit()
-
-    def apagar(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    @staticmethod
-    def get_all():
-        contatos = [
-            {"id": contato.id, "email": contato.email, "assunto": contato.assunto, "descricao": contato.descricao} for contato in Contato.query.all()
-        ]
-
-        return contatos
-
