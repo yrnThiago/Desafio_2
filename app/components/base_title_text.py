38b44c8ea8b3dca650a07d8a905804a8075962ac
yrnThiago <thiagodias_123@hotmail.com>
2023-11-02 12:52:45 -0300

    fix: add base style to title and text

--- app/components/base_title_text.py
+++ app/components/base_title_text.py
@@ -1,6 +1,6 @@
 def base_title_text(text_info: dict) -> str:
-    title_text_html = f"<h1>{text_info.get('title')}</h1>"
+    title_text_html = f"<h2 class='apresentacao__conteudo__texto_title text-center'>{text_info.get('title')}</h2>"
     for p in text_info.get('text'):
-        title_text_html += f"<p>{p}</p>"
+        title_text_html += f"<p class='apresentacao__conteudo__texto_subtitle text-center mt-3 mb-5'>{p}</p>"
 
     return title_text_html
