11b6ef422df5d61e16512055a8fc8356598be51e
Thiago Ribeiro <65740833+yrnThiago@users.noreply.github.com>
2023-11-07 10:04:44 -0300

    fix: remove 404 handler

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from app import create_app, db
-from flask import render_template
 
 config_name = "development"
 app = create_app(config_name)
-
-@app.errorhandler(404)
-def not_found(e):
-    return render_template('404_not_found.html')
 
 with app.app_context():
     db.create_all()
