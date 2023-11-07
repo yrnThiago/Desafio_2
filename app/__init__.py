a04f35d39a7523b41c374cbd2beb1520fb0b0e93
Thiago Ribeiro <65740833+yrnThiago@users.noreply.github.com>
2023-11-07 10:03:59 -0300

    feat: 404 handler to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from config.config import app_config
 from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
 
 db = SQLAlchemy()
+
+def page_not_found(e):
+    return render_template('404_not_found.html'), 404
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.json.sort_keys = False
     app.config.from_object(app_config[config_name])
+    app.register_error_handler(404, page_not_found)
 
     db.init_app(app)
 
