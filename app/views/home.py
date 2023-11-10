7fdd3e023fdf13f1cf68196dfa19ae93149f2f39
yrnThiago <thiagodias_123@hotmail.com>
2023-11-09 22:30:32 -0300

    fix: reasons to study param

--- app/views/home.py
+++ app/views/home.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, render_template, redirect, url_for
 from app.components.base_title_text import base_title_text
-from app.constants.home import ABOUT_TEXT
+from app.constants.home import ABOUT_TEXT, REASONS_TO_STUDY
 
 home_blueprint = Blueprint('home', __name__)
 
 
 @home_blueprint.route('/home')
 def home():
-    return render_template('home.html', about_university=base_title_text(ABOUT_TEXT))
+    return render_template('home.html', about_university=base_title_text(ABOUT_TEXT), reasons=REASONS_TO_STUDY)
 
 
 @home_blueprint.route('/')
