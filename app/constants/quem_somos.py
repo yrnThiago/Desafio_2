c6a94af11d1ff2fb3d7d329bf61ae3643fd6df60
yrnThiago <thiagodias_123@hotmail.com>
2023-11-09 22:29:09 -0300

    fix: remove reasons to study from quem_somos

--- app/constants/quem_somos.py
+++ app/constants/quem_somos.py
@@ -1,11 +1,3 @@
-reasons_to_study = [
-    {"title": "Material sempre atualizado", "text": "Texto 1", "icon": "fa fa-book fa-2xl"},
-    {"title": "Tablets em sala de aula", "text": "Texto 2", "icon": "fa fa-book fa-2xl"},
-    {"title": "Professores renomados", "text": "Texto 3", "icon": "fa fa-book fa-2xl"},
-    {"title": "Universidade bem avaliada", "text": "Texto 4", "icon": "fa fa-book fa-2xl"},
-    {"title": "Ótima localização", "text": "Texto 5", "icon": "fa fa-book fa-2xl"}
-]
-
 ABOUT_US_TEXT = {
     "title": "Quem somos",
     "text": [
