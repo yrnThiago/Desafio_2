def208ab70ee5ae2c92a60d54251cb3e79a07e0e
yrnThiago <thiagodias_123@hotmail.com>
2023-11-09 22:28:52 -0300

    fix: add reasons to study for home

--- app/constants/home.py
+++ app/constants/home.py
@@ -4,3 +4,11 @@ ABOUT_TEXT = {
         "Lorem <strong>ipsum dolor</strong> sit amet consectetur adipisicing elit. Sequi odit quibusdam hic exercitationem ut esse, quae consectetur facilis? Et odit obcaecati impedit voluptates corrupti cupiditate sequi, sint incidunt tenetur neque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi odit quibusdam hic exercitationem ut esse, quae consectetur facilis? Et odit obcaecati impedit voluptates corrupti cupiditate sequi, sint incidunt tenetur neque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi odit quibusdam hic exercitationem ut esse, quae consectetur facilis? Et odit obcaecati impedit voluptates corrupti cupiditate sequi, sint incidunt tenetur neque.", "Lorem <strong>ipsum dolor</strong> sit amet consectetur adipisicing elit. Sequi odit quibusdam hic exercitationem ut esse, quae consectetur facilis? Et odit obcaecati impedit voluptates corrupti cupiditate sequi, sint incidunt tenetur neque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi odit quibusdam hic exercitationem ut esse, quae consectetur facilis? Et odit obcaecati impedit voluptates corrupti cupiditate sequi, sint incidunt tenetur neque."
     ]
 }
+
+REASONS_TO_STUDY = [
+    {"title": "Material sempre atualizado", "text": "Texto 1", "icon": "fa fa-book fa-2xl"},
+    {"title": "Tablets em sala de aula", "text": "Texto 2", "icon": "fa fa-book fa-2xl"},
+    {"title": "Professores renomados", "text": "Texto 3", "icon": "fa fa-book fa-2xl"},
+    {"title": "Universidade bem avaliada", "text": "Texto 4", "icon": "fa fa-book fa-2xl"},
+    {"title": "Ótima localização", "text": "Texto 5", "icon": "fa fa-book fa-2xl"}
+]
