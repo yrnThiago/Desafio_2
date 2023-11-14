1ec8e0d0e337680bbff32819db4e104cb8a2c2c2
yrnThiago <thiagodias_123@hotmail.com>
2023-11-13 22:03:08 -0300

    fix: remove useless params

--- app/views/contato.py
+++ app/views/contato.py
@@ -9,9 +9,7 @@ contato_blueprint = Blueprint('contato', __name__)
 @contato_blueprint.route('/contato')
 def contato():
     all_contacts = get_many().json
-    return render_template("contato.html", contact=base_title_text(CONTACT_TEXT),
-                           input_email=base_input_text(EMAIL_INPUT), input_assunto=base_input_text(ASSUNTO_INPUT),
-                           input_descricao=base_input_text(DESCRICAO_INPUT, "textarea"), contacts=all_contacts)
+    return render_template("contato.html", contact=base_title_text(CONTACT_TEXT), contacts=all_contacts)
 
 
 @contato_blueprint.route('/contato/add', methods=["GET", "POST"])
