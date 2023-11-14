097f4bd2a7840a595a43fd731bad4644195d79da
yrnThiago <thiagodias_123@hotmail.com>
2023-11-13 22:00:47 -0300

    fix: remove useless file

--- app/components/base_input_text.py
+++
@@ -1,7 +0,0 @@
-def base_input_text(info: dict, input_type: str = "input"):
-    res = f'<label for="{info.get("label")}">{info.get("title")}:</label><br> <{input_type} class="formulario__input" '
-    for key, value in info.get("args").items():
-        res += f'{key}="{value}"'
-    res += f"></{input_type}>"
-
-    return res
