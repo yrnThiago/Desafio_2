30ed49942c78165548e37aebd1be479b10ac7358
yrnThiago <thiagodias_123@hotmail.com>
2023-11-13 21:56:59 -0300

    fix: change title

--- app/constants/contato.py
+++ app/constants/contato.py
@@ -1,9 +1,9 @@
-EMAIL_INPUT = {"tag": "input", "title": "Seu e-mail", "label": "email", "args": {"type": "email", "id": "email", "name": "email", "placeholder": "seuemail@email.com"}}
-ASSUNTO_INPUT = {"tag": "input", "title": "Assunto", "label": "assunto", "args": {"type": "text", "id": "assunto", "name": "assunto", "placeholder": "Assunto"}}
-DESCRICAO_INPUT = {"tag": "textarea", "title": "Descrição", "label": "descricao", "args": {"rows": "2", "cols": "32", "name": "descricao", "id": "descricao", "placeholder": "Isso é uma descrição..."}}
+EMAIL_INPUT = {"tag": "input", "title": "E-mail*", "label": "email", "args": {"type": "email", "id": "email", "name": "email", "placeholder": "seuemail@email.com"}}
+ASSUNTO_INPUT = {"tag": "input", "title": "Assunto*", "label": "assunto", "args": {"type": "text", "id": "assunto", "name": "assunto", "placeholder": "Assunto"}}
+DESCRICAO_INPUT = {"tag": "textarea", "title": "Descrição*", "label": "descricao", "args": {"rows": "2", "cols": "32", "name": "descricao", "id": "descricao", "placeholder": "Isso é uma descrição..."}}
 
 CONTACT_TEXT = {
-    "title": "Entre em contato",
+    "title": "Fale conosco",
     "text": [
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi odit quibusdam hic exercitationem ut esse, quae consectetur facilis?",
     ]
